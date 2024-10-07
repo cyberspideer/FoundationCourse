83b1e52516b021e423c57e20b2e7914aa0dce378
cyberspideer <zainabideen.pucad@gmail.com>
2024-10-07 19:57:19 +0300

    Simple Java script class task.

---
+++ Lab6/Site1/script.js
@@ -0,0 +1,7 @@
+//let answer = prompt ("what is your greatest achievment?")
+//alert(answer)
+//document.write
+//console.log
+//let whatEver = date.toString();
+let date = new (Date());
+console.log(date.toString());
